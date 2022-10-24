8fd9904e1f5f48325d8fe6f3988c416a5b5e009d
farrel.edwinconsultants <farrel.edwinconsultants@gmail.com>
2022-10-24 17:50:48 +0700

    Update UI (Sidebar, Styling, Logo)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.min.css'
+import 'antd/dist/antd.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
--- src/pages/MasterPembelian/Supplier/edit.jsx
+++ src/pages/MasterPembelian/Supplier/edit.jsx
@@ -1,50 +1,102 @@
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import axios from "axios";
-import React, { useEffect } from "react";
-import jsCookie from "js-cookie";
+import React, { useContext, useEffect, useRef } from "react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import Url from "../../../Config";
 import "./form.css";
-import SendIcon from "@mui/icons-material/Send";
-import Button from "@mui/material/Button";
-import EditIcon from '@mui/icons-material/Edit';
-import CancelIcon from "@mui/icons-material/Cancel";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import {
-  Box,
-  Checkbox,
-  IconButton,
-  Input,
-  Modal,
-  TextField,
-  Typography,
-} from "@mui/material";
-// import MaterialTable from "material-table";
 import { useSelector } from "react-redux";
-// import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-// import EditIcon from "@mui/icons-material/Edit";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import { TextField } from "@mui/material";
-// import CheckIcon from "@mui/icons-material/Check";
-import { PageHeader, Switch } from "antd";
+import { Button, Form, Input, PageHeader, Popconfirm, Skeleton, Switch, Table } from "antd";
+import { DeleteOutlined, PlusOutlined, SendOutlined } from "@ant-design/icons";
+import axios from "axios";
+
+const EditableContext = React.createContext(null);
+
+const EditableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
+
+const EditableCell = ({
+  title,
+  editable,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef(null);
+  const form = useContext(EditableContext);
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+
+  const toggleEdit = () => {
+    setEditing(!editing);
+    form.setFieldsValue({
+      [dataIndex]: record[dataIndex],
+    });
+  };
+
+  const save = async () => {
+    try {
+      const values = await form.validateFields();
+      toggleEdit();
+      handleSave({ ...record, ...values });
+    } catch (errInfo) {
+      console.log('Save failed:', errInfo);
+    }
+  };
+
+  let childNode = children;
+
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{
+          margin: 0,
+        }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{
+          paddingRight: 24,
+        }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  return <td {...restProps}>{childNode}</td>;
+};
 
 const EditSupplier = () => {
-  // const auth.token = jsCookie.get("auth");
   const auth = useSelector(state => state.auth);
   const [code, setCode] = useState('');
   const [name, setName] = useState('');
   const [bussiness_ent, setBussiness_ent] = useState('');
-  const [grup,setGrup] = useState('');
+  const [grup, setGrup] = useState('');
   const [phone_number, setPhone_number] = useState('');
   const [email, setEmail] = useState('');
   const [npwp, setNpwp] = useState('');
@@ -52,36 +104,140 @@ const EditSupplier = () => {
   const [checked, setChecked] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
-
-  const [alamat, setAlamat] = useState([]);
-  const [kota, setKota] = useState([]);
-  const [kecamatan, setKecamatan] = useState([]);
-  const [kelurahan, setKelurahan] = useState([]);
-  const [kode_pos, setKode_pos] = useState([]);
+  const [count, setCount] = useState(2);
+  const [loading, setLoading] = useState(true);
 
   // const [getCustomer, setGetCustomer] = useState();
 
-  const [address, setAddress] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
 
-  const [data, setData] = useState([]);
+  const handleAdd = () => {
+    const newData = {
+      key: count,
+      name: ``,
+      age: '',
+      address: ``,
+    };
+    setDataSource([...dataSource, newData]);
+    setCount(count + 1);
+  };
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const handleDelete = (key) => {
+    const newData = dataSource.filter((item) => item.key !== key);
+    setDataSource(newData);
+  };
+
+  const handleSave = (row) => {
+    const newData = [...dataSource];
+    const index = newData.findIndex((item) => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, { ...item, ...row });
+    setDataSource(newData);
+  };
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+
+  const defaultColumns = [
+    {
+      title: 'No.',
+      dataIndex: 'index',
+      width: '3%',
+      align: 'center',
+      render: (text, record, index) => index + 1
+    },
+    {
+      title: 'Alamat',
+      dataIndex: 'address',
+      width: '30%',
+      editable: true,
+    },
+    {
+      title: 'Kelurahan',
+      editable: true,
+      dataIndex: 'urban_village',
+    },
+    {
+      title: 'Kecamatan',
+      editable: true,
+      dataIndex: 'sub_district',
+    },
+    {
+      title: 'Kota',
+      editable: true,
+      dataIndex: 'city',
+    },
+    {
+      title: 'Kode Pos',
+      editable: true,
+      dataIndex: 'postal_code',
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      align: 'center',
+      width: '5%',
+      render: (_, record) =>
+        dataSource.length >= 1 ? (
+          <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+            <Button
+              size='small'
+              type="danger"
+              icon={<DeleteOutlined />}
+            />
+          </Popconfirm>
+        ) : null,
+    },
+  ];
+
+  const columns = defaultColumns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
 
   const handleUpdate = async (e) => {
     e.preventDefault();
-    const userData = new URLSearchParams();
+    const userData = new FormData();
     userData.append("nama", name);
     userData.append("badan_usaha", bussiness_ent);
+    userData.append("grup", grup);
     userData.append("nomor_telepon", phone_number);
     userData.append("email", email);
     userData.append("npwp", npwp);
-    userData.append("grup", grup);
     userData.append("status", status);
+
+    dataSource.map((address) => {
+      console.log(address);
+      userData.append("alamat[]", address.address);
+      userData.append("kota[]", address.city);
+      userData.append("kecamatan[]", address.sub_district);
+      userData.append("kelurahan[]", address.urban_village);
+      userData.append("kode_pos[]", address.postal_code);
+    });
+
+    // for (var pair of userData.entries()) {
+    //   console.log(pair[0] + ", " + pair[1]);
+    // }
 
     axios({
       method: "put",
-      url: `${Url}/suppliers/${id}`,
+      url: `${Url}/suppliers`,
       data: userData,
       headers: {
         Accept: "application/json",
@@ -89,51 +245,23 @@ const EditSupplier = () => {
       },
     })
       .then(function (res) {
-        const userData = new FormData();
-        address.map((address) => {
-          userData.append("alamat[]", address.address);
-          userData.append("kota[]", address.city);
-          userData.append("kecamatan[]", address.sub_district);
-          userData.append("kelurahan[]", address.urban_village);
-          userData.append("kode_pos[]", address.postal_code);
-        });
-        axios({
-          method: "post",
-          url: `${Url}/supplier_addresses/${id}`,
-          data: userData,
-          headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${auth.token}`,
-          },
-        })
-          .then(function (res) {
-            //handle success
-            console.log(res);
-            Swal.fire("Alamat Berhasil Ditambahkan", `Alamat ${code} Berhasil Ditambah`, "success");
-            navigate(`/supplier`);
-            // setTimeout(window.location.reload.bind(window.location), 600);
-          })
-          .catch((err) => {
-            if (err.response) {
-              console.log("err.response ", err.response);
-              Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: err.response.data.error.nama,
-              });
-            } else if (err.request) {
-              console.log("err.request ", err.request);
-              Swal.fire("Gagal Ditambahkan", "Mohon Cek Dahulu..", "error");
-            } else if (err.message) {
-              // do something other than the other two
-              Swal.fire("Gagal Ditambahkan", "Mohon Cek Dahulu..", "error");
-            }
-          });
+        //handle success
+        console.log(res);
+        Swal.fire(
+          "Berhasil Ditambahkan",
+          `${getSupplier} Masuk dalam list`,
+          "success"
+        );
+        navigate("/supplier");
       })
       .catch((err) => {
         if (err.response) {
           console.log("err.response ", err.response);
-          Swal.fire("Gagal Ditambahkan", "Mohon Cek Dahulu..", "error");
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: err.response.data.error,
+          });
         } else if (err.request) {
           console.log("err.request ", err.request);
           Swal.fire("Gagal Ditambahkan", "Mohon Cek Dahulu..", "error");
@@ -144,31 +272,17 @@ const EditSupplier = () => {
       });
   };
 
-  const columns = [
-    { title: "ID", field: "id", editable: false },
-    { title: "Alamat", field: "address" },
-    { title: "Kelurahan", field: "urban_village" },
-    { title: "Kecamatan", field: "sub_district" },
-    { title: "Kota", field: "city" },
-    { title: "Kode Pos", field: "postal_code" },
-  ];
-
-  const handleAddress = async (e) => {
-    e.preventDefault();
-   
-  }
-
   const onChange = () => {
     checked ? setChecked(false) : setChecked(true)
 
     if (checked === false) {
-        setStatus("Active");
-        // console.log('Active');
+      setStatus("Active");
+      // console.log('Active');
     } else {
-        setStatus("Inactive");
-        // console.log('Inactive');
+      setStatus("Inactive");
+      // console.log('Inactive');
     }
-};
+  };
 
   useEffect(() => {
     getSupplierById()
@@ -182,7 +296,6 @@ const EditSupplier = () => {
       },
     })
       .then((res) => {
-        setData(res.data.data[0]);
         const getData = res.data.data[0];
         setCode(getData.code);
         setName(getData.name);
@@ -192,7 +305,8 @@ const EditSupplier = () => {
         setNpwp(getData.npwp);
         setGrup(getData._group);
         setStatus(getData.status);
-        setAddress(getData.supplier_addresses);
+        setDataSource(getData.supplier_addresses);
+        setLoading(false)
       })
       .catch((err) => {
         // Jika Gagal
@@ -200,19 +314,27 @@ const EditSupplier = () => {
       });
   }
 
-
+  if (loading) {
+    return (
+      <>
+        <form className="p-3 mb-3 bg-body rounded">
+          <Skeleton active />
+        </form>
+        <form className="p-3 mb-3 bg-body rounded">
+          <Skeleton active />
+        </form>
+      </>
+    )
+  }
 
   return (
     <>
-      <form className="  p-3 mb-4 bg-body rounded">
-        <div className="text-title text-start mb-4">
-                    <PageHeader
-                        ghost={false}
-                        onBack={() => window.history.back()}
-                        title="Edit Supplier">
-                     </PageHeader>
-          {/* <h3 className="title fw-bold">Edit Supplier</h3> */}
-        </div>
+      <PageHeader
+        ghost={false}
+        className="bg-body rounded mb-2"
+        onBack={() => window.history.back()}
+        title="Edit Supplier"
+      >
         <div className="row mb-3">
           <label htmlFor="inputKode3" className="col-sm-2 col-form-label">
             Kode
@@ -318,7 +440,7 @@ const EditSupplier = () => {
             >
               <option>Pilih Grup</option>
               <option value="Lokal" selected={grup === "Lokal"}>
-              Lokal
+                Lokal
               </option>
               <option value="Impor" selected={grup === "Impor"}>
                 Impor
@@ -336,136 +458,43 @@ const EditSupplier = () => {
                   : "Nonaktif"
               }
             </label>
-            </div>
           </div>
- 
+        </div>
+      </PageHeader>
 
-        {/* <fieldset className="row mb-3">
-          <legend className="col-form-label col-sm-2 pt-0">Status</legend>
-          <div className="col-sm-10">
-            <div className="form-check">
-              <input
-                onChange={(e) => setStatus(e.target.value)}
-                value="Active"
-                checked={status === "Active"}
-                className="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                id="flexRadioDefault1"
-              />
-              <label className="form-check-label" htmlFor="gridRadios1">
-                Aktif
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                onChange={(e) => setStatus(e.target.value)}
-                value="Inactive"
-                checked={status === "Inactive"}
-                className="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                id="flexRadioDefault2"
-              />
-              <label className="form-check-label" htmlFor="gridRadios2">
-                Non-Aktif
-              </label>
-            </div>
-          </div>
-        </fieldset> */}
-        <div className="d-grid mt-3 gap-2 d-md-flex justify-content-md-end">
-          {/* <button onClick={handleUpdate} className="btn btn-primary" type="button">
-            Simpan <SendIcon className="ms-1" />
-          </button> */}
-          {/* <Button
-          onClick={handleSubmit}
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          Simpan
-        </Button> */}
-        </div>
-      </form>
-      <form className="  p-3 mb-3 bg-body rounded">
-        {/* <MaterialTable
-          title="Alamat Pelanggan"
-          data={address}
+      <PageHeader
+        ghost={false}
+        className="bg-body rounded"
+        title="Tambah Alamat Supplier"
+        extra={[
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={handleAdd}
+            style={{
+              marginBottom: 16,
+            }}
+          />
+        ]}
+      >
+        <Table
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          dataSource={dataSource}
           columns={columns}
-          onChange={(e) => setAddress(e.target.value)}
-          editable={{
-            onRowAdd: (newRow) =>
-              new Promise((resolve, reject) => {
-                const updatedRows = [
-                  ...address,
-                  { id: address.length + 1, ...newRow },
-                ];
-                setTimeout(() => {
-                  setAddress(updatedRows);
-                  resolve();
-                }, 2000);
-              }),
-            onRowDelete: (selectedRow) =>
-              new Promise((resolve, reject) => {
-                const index = selectedRow.tableData.id;
-                const updatedRows = [...address];
-                updatedRows.splice(index, 1);
-                setTimeout(() => {
-                  setAddress(updatedRows);
-                  resolve();
-                }, 2000);
-              }),
-            onRowUpdate: (updatedRow, oldRow) =>
-              new Promise((resolve, reject) => {
-                const index = oldRow.tableData.id;
-                const updatedRows = [...address];
-                updatedRows[index] = updatedRow;
-                setTimeout(() => {
-                  setAddress(updatedRows);
-                  resolve();
-                }, 2000);
-              }),
-            onBulkUpdate: (selectedRows) =>
-              new Promise((resolve, reject) => {
-                const rows = Object.values(selectedRows);
-                const updatedRows = [...address];
-                let index;
-                rows.map((emp) => {
-                  index = emp.oldData.tableData.id;
-                  updatedRows[index] = emp.newData;
-                });
-                setTimeout(() => {
-                  setAddress(updatedRows);
-                  resolve();
-                }, 2000);
-              }),
-          }}
-          options={{
-            actionsColumnIndex: -1,
-            addRowPosition: "first",
-          }}
-        /> */}
-        <div className="d-grid mt-3 gap-2 d-md-flex justify-content-md-end">
-          <button onClick={handleUpdate} className="btn btn-primary" type="button">
-            Simpan <SendIcon className="ms-1" />
-          </button>
-          {/* <Button
-          onClick={handleSubmit}
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          Simpan
-        </Button> */}
+        />
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-2">
+          <Button
+            type="primary"
+            icon={<SendOutlined />}
+            size="large"
+            onClick={handleUpdate}
+          >
+            Submit
+          </Button>
         </div>
-      </form>
-      {/* <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-        <Button
-          onClick={handleUpdate}
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          Simpan
-        </Button>
-      </div> */}
+      </PageHeader>
     </>
   );
 };
