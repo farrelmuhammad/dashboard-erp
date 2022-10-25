7b9a0bc50d17713c51906b74509e43532e9772d8
farrel.edwinconsultants <farrel.edwinconsultants@gmail.com>
2022-10-25 10:16:29 +0700

    Update

--- src/components/moleculles/TipeProdukTable/index.jsx
+++ src/components/moleculles/TipeProdukTable/index.jsx
@@ -154,12 +154,12 @@ const TipeProdukTable = () => {
           ellipsis={
             ellipsis
               ? {
-                tooltip: toTitleCase(text),
+                tooltip: text,
               }
               : false
           }
         >
-          {toTitleCase(text)}
+          {text}
         </Text>
       )
       // sorter: (a, b) => a.customer_id.length - b.customer_id.length,
