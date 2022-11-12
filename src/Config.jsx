e7b87a2539bee2f192a84d0d2bb7eb1be20e7f23
Meggy <mega.edwinconsultants@gmail.com>
2022-11-12 17:47:26 +0700

    build

--- src/Config.jsx
+++ src/Config.jsx
@@ -3,6 +3,6 @@
 // const Url = 'http://192.168.100.106:8000/api'
 // const Url = 'http://192.168.0.108:8000/api'
 //const Url = 'http://localhost:8000/api'
- const Url = 'https://api-edwin-erp-dev.edwinconsultants.com/api'
+ const Url = 'https://edwin-erp-be.edwinconsultants.com/api'
 
 export default Url
